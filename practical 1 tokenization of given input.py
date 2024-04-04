381323f3985b23fe653e7be9445c89b20d052422
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-04 19:53:20 +0530

    Update practical 1 tokenization of given input.py

--- practical 1 tokenization of given input.py
+++ practical 1 tokenization of given input.py
@@ -4,18 +4,13 @@ import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 def tokenize_text(text):
-    
     sentences = sent_tokenize(text)
-    
     words = [word_tokenize(sentence) for sentence in sentences]
     return sentences, words
 
 if __name__ == "__main__":
-    
-    input_text = "This is an example sentence. Tokenization is the process of breaking text into words."
-    
+    input_text = "This is an example sentence.Tokenization is the process of breaking text into words."
     sentences, words = tokenize_text(input_text)
-    
     print("Original text:")
     print(input_text)
     print("\nTokenized sentences:")
