c7e23b8512281642e7be6bb1bae519fbad6fa9eb
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-04 20:54:19 +0530

    Update practical 1 tokenization of given input.py

--- practical 1 tokenization of given input.py
+++ practical 1 tokenization of given input.py
@@ -18,4 +18,50 @@ if __name__ == "__main__":
         print(f" Sentence {i}: {sentence}")
     print("\nTokenized words:")
     for i, sentence_words in enumerate(words, start=1):
-        print(f" Sentence {i}: {sentence_words}")
+        print(f" Sentence {i}: {sentence_words}")\
+
+
+# --------------------------------------------------------------
+
+
+from typing import List
+
+def fillSentence(user_input) -> List[str]:
+    sentence: List[str] = []
+    common: str = ""
+    for i in user_input:
+        if i == ".":
+            sentence.append(common)
+            common = ""
+        if i != ".":
+            common += i
+    if sentence == True:
+        sentence.append(common)
+    return sentence
+    
+def fillWords(user_input) -> List[str]:
+    words: List[str] = []
+    common: str = ""
+    for i in user_input:
+        if i == " ":
+            words.append(common)
+            common = ""
+        if i != " ":
+            common += i
+    if words == True:
+        words.append(common)
+    return words
+            
+def tokenization() -> List[str]:
+    user_input: str = input("Enter your prompt:\n")
+    sentenceList: List[str] = fillSentence(user_input)
+    wordList: List[str] = fillWords(user_input)
+    
+    return sentenceList, wordList
+    
+def main() -> None:
+    store_tokenization = tokenization()
+    print(store_tokenization)
+    
+if __name__ == "__main__":
+    main()
